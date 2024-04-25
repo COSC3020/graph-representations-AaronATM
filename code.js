de9fe074914707d9cd8d2f52a0c3a01f5cd71ca8
Aaron Krapes <aaronkrapes@outlook.com>
2024-04-24 18:29:24 -0600

    updated code.js

--- code.js
+++ code.js
@@ -1,3 +1,32 @@
-function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+// used examples from geeks for geeks
+// https://www.geeksforgeeks.org/convert-adjacency-matrix-to-adjacency-list-representation-of-graph/
+
+function convertToAdjList(adjMatrix) 
+{
+    var list = new Array(adjMatrix.length);
+
+    for (let i = 0; i < adjMatrix.length; i++) 
+    {
+        for (let j = 0; j < adjMatrix[i].length; j++) 
+        {
+            if (adjMatrix[i][j] != 0) 
+            {
+                if (!list[i]) 
+                {
+                    list[i] = [];
+                }
+                list[i].push(j);
+            }
+        }
+    }
+    return list;
 }
+
+
+var input = [];
+var input2 = [[0, 0, 1], [0, 0, 1], [1, 1, 0]]
+
+var output = convertToAdjList(input);
+var output2 = convertToAdjList(input2);
+console.log(output);
+console.log(output2);
